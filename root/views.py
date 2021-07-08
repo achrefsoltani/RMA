b63472857071eb7812b19f4e20db7fbf3e39d93d
Achref Soltani <60237898+achrefsoltani@users.noreply.github.com>
2021-07-08 11:37:37 +0100

    homepage modifications

--- root/views.py
+++ root/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
+
+date = datetime.now
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'root/home.html', {})
+    return render(request, 'root/home.html', {'date':date})
