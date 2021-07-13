6c4256317817a25d8f56653570013a5edad813b4
Achref Soltani <60237898+achrefsoltani@users.noreply.github.com>
2021-07-13 11:09:52 +0100

    session __str__ fix

--- root/admin.py
+++ root/admin.py
@@ -1,3 +1,34 @@
+import actif.models
+import impact.models
+import menace.models
+import mesure.models
+import session.models
+import vulnerabilite.models
+
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(actif.models.typeActif)
+admin.site.register(actif.models.actif)
+admin.site.register(actif.models.actifCritique)
+
+admin.site.register(impact.models.typeImpact)
+admin.site.register(impact.models.impact)
+admin.site.register(impact.models.impactNote)
+
+admin.site.register(menace.models.menace)
+
+
+admin.site.register(mesure.models.mesure)
+
+
+admin.site.register(session.models.session)
+
+
+admin.site.register(vulnerabilite.models.typeVulnerabilite)
+admin.site.register(vulnerabilite.models.vulnerabilite)
+admin.site.register(vulnerabilite.models.vulnerabiliteNote)
+
+
+
