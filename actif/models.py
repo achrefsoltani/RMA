cc0f7e9d202cc88e73dcf3db647170875e486eee
khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2021-07-14 02:15:34 +0200

    actif

--- actif/models.py
+++ actif/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class TypeActif(models.Model):
+    reference = models.CharField(max_length=150)
+    description= models.TextField(blank=True)
+class Actif(models.Model):
+    reference = models.CharField(max_length=150)
+    description= models.TextField(blank=True)
+    criticiteAffaire = models.IntegerField()
+    cid = models.IntegerField()
+    proprietaire = models.CharField(max_length=150)
+    #relation
+     typeActif = models.CharField(max_length=150)
+     actifRelation = models.CharField(max_length=150)
+     mesure = models.CharField(max_length=150)
+     #logs
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+
+
