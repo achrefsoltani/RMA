7d5c01147c1a2f0daabef81b651cf2ca8298e50c
khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2021-09-13 21:37:15 +0200

    updates

--- vulnerabilite/views.py
+++ vulnerabilite/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import vulnerabilite
+from .models import typeVulnerabilite
 
 date = datetime.now
 
 # Create your views here.
 
 def list(request):
-    return render(request, 'vulnerabilite/list.html', {'date':date})
+
+    vulnerabilites = vulnerabilite.objects.all()
+    typesVulnerabilites =  typeVulnerabilite.objects.all()
+                               
+    return render(request, 'vulnerabilite/list.html', {'all':vulnerabilites, 'allType':typesVulnerabilites})
