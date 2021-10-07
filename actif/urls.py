a8261dcacc369f1af403fee06fe4509de3f59f5e
khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2021-10-08 00:33:05 +0200

    typeactif

--- actif/urls.py
+++ actif/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name="listActifs"),
+    path('<str:type>/', views.listByType, name="listActifType"),
+  
 ]
--- actif/views.py
+++ actif/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import datetime
 from .models import actifCritique
 from .models import actif
+from .models import typeActif
 from .filters import ActifFilter
 from django.core.paginator import Paginator
 
@@ -10,13 +11,14 @@ date = datetime.now
 # Create your views here.
 
 def list(request):
+    typeactifs= typeActif.objects.all()
     if 'search' in request.GET:
         search=request.GET['search']
         actifs=actif.objects.filter(description__icontains=search)
     else:    
         actifs= actif.objects.all()
       
-    paginator= Paginator(actifs, per_page=3)
+    paginator= Paginator(actifs, per_page=10)
     page_number= request.GET.get('page', 1)
     page_obj= paginator.get_page(page_number)
     return render(
@@ -25,6 +27,18 @@ def list(request):
         {
             'all':page_obj.object_list,
             'paginator':paginator,
-            'page_number': int(page_number)
+            'page_number': int(page_number),
+            'typeactifs': typeactifs
+        })
+
+def listByType(request, type):  
+    actifsByTypes= actif.objects.filter(type=type)
+
+    return render(
+        request, 
+        'actif/list.html',
+        {
+            
+         'actifsByType': actifsByTypes
         }
     )
