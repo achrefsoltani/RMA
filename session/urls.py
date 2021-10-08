d4f56238699282963e5c3e1b6a2b120bb09ad731
Achref Soltani <60237898+achrefsoltani@users.noreply.github.com>
2021-10-08 02:30:47 +0100

    urls/pages session

--- session/urls.py
+++ session/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name="listSessions"),
+
+    path('<str:pk>/choix', views.choix, name="detailSession"),
+    path('<str:pk>/listActifs', views.listActifC, name="actifsSession"),
+    path('<str:pk>/vuln', views.vulnerabilites, name="vulSession"),
+    path('<str:pk>/menaces', views.menaces, name="menaceSession"),
+    path('<str:pk>/traitement', views.traitement, name="traitementSession"),
+    path('<str:pk>/rapport', views.rapport, name="rapportSession"),
+
+    path('<str:pk>/', views.detail, name="detailSession"),
 ]
--- session/views.py
+++ session/views.py
@@ -6,6 +6,35 @@ date = datetime.now
 
 # Create your views here.
 
-def list(request):
-    all_sessions= session.objects.all()
-    return render(request, 'session/list.html', {'date':date},{'all':all_sessions})
+def list(request , **args):
+
+    return render(request, 'session/list.html', {'date':date})
+
+def detail(request , **args):
+
+    return render(request, 'session/detail.html', {'date':date})
+
+def choix(request, **args):
+
+    return render(request, 'session/choix.html', {'date':date})
+    
+def listActifC(request, **args):
+
+    return render(request, 'session/listActifs.html', {'date':date})
+    
+def vulnerabilites(request, **args):
+
+    return render(request, 'session/Vulnerabilites.html', {'date':date})
+    
+def menaces(request, **args):
+
+    return render(request, 'session/menaces.html', {'date':date})
+    
+def traitement(request, **args):
+
+    return render(request, 'session/traitement.html', {'date':date})
+    
+def rapport(request, **args):
+
+    return render(request, 'session/rapport.html', {'date':date})
+    
