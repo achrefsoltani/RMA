5ce27a85e0a2b69ff5820e12662e748a5967eac1
khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2021-10-11 23:42:10 +0200

    updates

--- vulnerabilite/views.py
+++ vulnerabilite/views.py
@@ -19,7 +19,8 @@ def list(request):
     page_number= request.GET.get('page', 1)
     page_obj= paginator.get_page(page_number)
                                
-    return render(request, 'vulnerabilite/list.html', { 'allType':typesVulnerabilites,
+    return render(request, 'vulnerabilite/list.html', { 
+        'allType':typesVulnerabilites,
         'all':page_obj.object_list,
         'paginator':paginator,
         'page_number': int(page_number),
